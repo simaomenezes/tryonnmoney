10b0c14ddd5a0f4b838ac3b68c06690f860a6098
Simão Menezes <simaomenezesneto@gmail.com>
2018-03-08 23:11:04 -0300

    Deleteando pessoa e tratando caso a pessoa nao exista mais

--- src/main/java/com/tryonnmoney/exeptionhandler/TryonnmoneyExceptionHandler.java
+++ src/main/java/com/tryonnmoney/exeptionhandler/TryonnmoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -62,6 +65,24 @@ public class TryonnmoneyExceptionHandler extends ResponseEntityExceptionHandler 
 		
 
 		return erros;
+	}
+	
+	
+	
+	/*
+	 * Tratando execption e customizando
+	 * 
+	 * utilizado quando vai deleta um registro e o mesmo nao existe mais
+	 * */
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	//@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest webRequest) {
+	
+		String messagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(messagemUsuario, mensagemDesenvolvedor));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
 	}
 
 	public static class Erro {
