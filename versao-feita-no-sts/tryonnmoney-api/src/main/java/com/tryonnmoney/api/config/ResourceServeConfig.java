9c340384ce0635605f869aad4c3fad85f331b0a4
Simão Menezes <simaomenezesneto@gmail.com>
2018-03-14 00:34:52 -0300

    Implementando authorizacao.

---
+++ versao-feita-no-sts/tryonnmoney-api/src/main/java/com/tryonnmoney/api/config/ResourceServeConfig.java
@@ -0,0 +1,47 @@
+package com.tryonnmoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/*
+ * Utilizando autenticacao com Oauth2
+ * 
+ * */
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServeConfig extends ResourceServerConfigurerAdapter{
+
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("ROLES");
+	}
+	
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/categorias").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.csrf().disable();
+	}
+	
+	/*
+	 * Nao vai manter sessao no servidor
+	 * */
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+}
